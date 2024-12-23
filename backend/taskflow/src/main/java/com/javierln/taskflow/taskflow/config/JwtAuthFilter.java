c070a7afd22cb63950177f638eba2c0581039d2b
javierlnc <javierlnc@icloud.com>
2024-12-23 09:27:09 -0500

    end JwtAuthFilter

--- backend/taskflow/src/main/java/com/javierln/taskflow/taskflow/config/JwtAuthFilter.java
+++ backend/taskflow/src/main/java/com/javierln/taskflow/taskflow/config/JwtAuthFilter.java
@@ -2,8 +2,16 @@ package com.javierln.taskflow.taskflow.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.javierln.taskflow.taskflow.service.UserService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -33,6 +42,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         }
         final String token = authorizationHeader.substring(7);
         final String username = jwtUtil.extractUsername(token);
+        if (username == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (jwtUtil.isTokenValid(token, userDetails)) {
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            context.setAuthentication(authToken);
+            SecurityContextHolder.setContext(context);
+        }
+        filterChain.doFilter(request, response);
 
     }
 
