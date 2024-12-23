842ccb5b3f03bcb1c40f833722d114aeadb73489
javierlnc <javierlnc@icloud.com>
2024-12-23 09:06:00 -0500

    Create jwtUtil

--- backend/taskflow/src/main/java/com/javierln/taskflow/taskflow/config/JwtUtil.java
+++ backend/taskflow/src/main/java/com/javierln/taskflow/taskflow/config/JwtUtil.java
@@ -1,10 +1,80 @@
 package com.javierln.taskflow.taskflow.config;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 public class JwtUtil {
 
-    public String extractUsername(String token) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'extractUsername'");
+    @Value("${jwt.secretKey}")
+    private String PRIVATE_KEY;
+    @Value("${jwt.expirationTime}")
+    private Long EXPIRATION_TIME;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(PRIVATE_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(getExpirationDate())
+                .claims().add(extraClaims).and()
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Date getExpirationDate() {
+        return new Date(System.currentTimeMillis() + EXPIRATION_TIME);
+    }
 }
